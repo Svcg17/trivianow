69651c5015d927ca6059696e957ba67c998abfd4
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-31 01:00:02 -0700

    Costumizing Homepage component to have a grid layout

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Typography, Row, Col } from 'antd';
+import './homepage.css';
 
 /** Displays Trivia Now's homepage */
 const Homepage = ({ history }) => {
+  const { Title } = Typography;
+
   return(
-    <div className='Homepage'>
-      <div>Trivia Now Logo</div>
-      <Button onClick={() => history.push('/trivia')}>Start Now</Button>
+    <div className='homepage'>
+      <Row align='middle' justify='center'>
+        <Col span={24}>
+          <Title>Trivia Now</Title>
+          <Title level={2}>Ready to level up your trivia skills?</Title>
+        </Col>
+       <Col span={24}>
+          <Button size='large' onClick={() => history.push('/trivia')}>Start Now</Button>
+        </Col>
+      </Row>
     </div>
   )
 };
