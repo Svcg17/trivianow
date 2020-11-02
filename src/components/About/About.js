48199e3d86514c41dd43e437c7194942d48109ba
Sofia Cheung <sofiavictoria17@gmail.com>
2020-11-01 20:30:52 -0800

    Creating an About component

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <p>Trivia Now is a React web application where you get to improve your trivia knowledge</p>
+    <p>It is also my coding challenge for Tandem's Apprentice Software Engineer application.</p>
+  </div>
+);
+
+export default About;
